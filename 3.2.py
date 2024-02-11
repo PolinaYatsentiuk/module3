8434828833ada8c0028afd0fbf4aa120095450ca
PolinaYatsentiuk <p.yatsentyuk@gmail.com>
2024-02-11 12:02:23 +0200

    Fixed

--- 3.2.py
+++ 3.2.py
@@ -1,11 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    if max <= min or quantity > max or max-min+1<quantity:
+        return []
+
     for num in [min, max, quantity]:
         if not (1 <= num <= 1000):
             return []
     unique_numbers = random.sample(range(min,max+1), quantity)
     return sorted(unique_numbers)
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(15, 100, 24)
 print("Ваші лотерейні числа:", lottery_numbers)
